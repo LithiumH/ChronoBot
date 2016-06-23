fa762330add662adec37bada9c1bbde3175ab21e
James Uejio <james.uejio@appdynamics.com>
2016-06-23 15:36:26 -0700

    Merge conflict resolved

--- bot.py
+++ bot.py
@@ -5,119 +5,128 @@ import json
 import time
 import datetime
 import pickle
+import pymongo
+from pymongo import MongoClient
 from timeSheetMaker import *
 from constants import *
 from models.user import *
+from chrono import *
 
 class Listener(object):
-	def __init__(self):
-		# self.state = '' # state can be timesheet-init, or an empty string
-		self.chrono = False
-		self.user_map = {}
+    def __init__(self):
+        # self.state = '' # state can be timesheet-init, or an empty string
+        self.chrono = False
+        self.user_map = {}
 
+    def start(self):
+        token = 'xoxb-53468281812-uwQohEw49nWfhcy8N2myHv8H'
+        url = 'https://slack.com/api/rtm.start'
+        response = requests.get(url + '?token=' + token)
+        payload = json.loads(response.text)
+        wws = payload['url']
+        async def main():
+            async with websockets.connect(wws) as websocket:
+                self.websocket = websocket
+                while True:
+                    greeting = await websocket.recv()
+                    event = json.loads(greeting)
+                    print(event)            
+                    if 'type' in event and event['type'] == 'message':
+                        text = event['text']
+                        if '<@U1KDS89PW>' in text or event['channel'] == 'D1KDUJZFG':
+                            self.chrono = True
+                        if self.chrono:
+                            unique_id = event['user']
+                            channel = event['channel']
+                            ping = '<@{}> '.format(unique_id)
+                            if event['channel'] == 'D1KDUJZFG':
+                                ping = ''
+                            if unique_id not in self.user_map:
+                                if 'register me' in text:
+                                    self.user_map[unique_id] = User(unique_id)
+                                    await websocket.send(self.make_json(channel, ping + 'What is your full name?'))
+                                else:
+                                    await websocket.send(self.make_json(channel, ping + 'You seem to not be in the database, please type register me.'))
+                            else:
+                                user = self.user_map[unique_id]
+                                step = user.step
+                                if user.state == '':
+                                    user.state, new_text = self.switch_state(text, ping)
+                                    await websocket.send(self.make_json(channel, new_text))
+                                elif user.state == 'register me':
+                                    if step == 0:
+                                        user.name = text
+                                        await websocket.send(self.make_json(channel, ping + 'What is your email?'))
+                                    if step == 1:
+                                        user.email = text
+                                        await websocket.send(self.make_json(channel, ping + 'What is your manager\'s first name'))
+                                    elif step == 2:
+                                        user.manager = text
+                                        await websocket.send(self.make_json(channel, ping + 'What is your Team?'))
+                                    elif step == 3:
+                                        user.team = text
+                                        await websocket.send(self.make_json(channel, ping + 'What is your role?'))
+                                    elif step == 4:
+                                        user.role = text
+                                        await websocket.send(self.make_json(channel, ping + 'Registration complete! Name: {}, Email: {}, Manager: {}, Team: {}, Role: {}'.format(user.name, user.email, user.manager, user.team, user.role)))
+                                        user.state = ''
+                                    user.step += 1
+                                elif user.state == 'timesheet-init':
+                                    if text == 'yes':
+                                        date = datetime.date.today()
+                                        new_date = lastday(date, 'sunday')
+                                        path = self.generate_default(user.name, new_date)
+                                        send_email(user.name,new_date, 'myTimeSheet.xlsx', path, user.manager)
+                                        await websocket.send(self.make_json(channel, ping + 'BOOM! Your timesheet is sent'))
+                                    else:
+                                        args = text.split(' ')
+                                        if len(args) != 6:
+                                            await websocket.send(self.make_json(channel, ping + 'Sorry, wrong syntax. Please start over'))
+                                        else:
+                                            path = generate_specific(user.name, args[0], args[1], args[2], args[3], args[4], args[5])
+                                            date = datetime.datetime.strptime(args[0], '%m-%d-%Y')
+                                            send_email(user.name, lastday(date, 'sunday'), 'myTimeSheet.xlsx', path, user.manager)
+                                    user.state = ''
+                                elif user.state == 'quit':
+                                    pass
+                                elif user.state == 'faq':
+                                    await websocket.send(self.make_json(channel, ping + get_answer(text)))
 
-	def start(self):
-		token = 'xoxb-53468281812-uwQohEw49nWfhcy8N2myHv8H'
-		url = 'https://slack.com/api/rtm.start'
-		response = requests.get(url + '?token=' + token)
-		payload = json.loads(response.text)
-		wws = payload['url']
-		async def main():
-			async with websockets.connect(wws) as websocket:
-				self.websocket = websocket
-				while True:
-					greeting = await websocket.recv()
-					event = json.loads(greeting)
-					print(event)			
-					if 'type' in event and event['type'] == 'message':
-						text = event['text']
-						if '<@U1KDS89PW>' in text or event['channel'] == 'D1KDUJZFG':
-							self.chrono = True
-						if self.chrono:
-							unique_id = event['user']
-							channel = event['channel']
-							ping = '<@{}> '.format(unique_id)
-							if event['channel'] == 'D1KDUJZFG':
-								ping = ''
-							if unique_id not in self.user_map:
-								if 'register me' in text:
-									self.user_map[unique_id] = User(unique_id)
-									await websocket.send(self.make_json(channel, ping + 'What is your full name?'))
-								else:
-									await websocket.send(self.make_json(channel, ping + 'You seem to not be in the database, please type register me.'))
-							else:
-								user = self.user_map[unique_id]
-								step = user.step
-								if user.state == '':
-									user.state, new_text = self.switch_state(text, ping)
-									await websocket.send(self.make_json(channel, new_text))
-								elif user.state == 'register me':
-									if step == 0:
-										user.name = text
-										await websocket.send(self.make_json(channel, ping + 'What is your manager\'s first name'))
-									elif step == 1:
-										user.manager = text
-										await websocket.send(self.make_json(channel, ping + 'What is your Team?'))
-									elif step == 2:
-										user.team = text
-										await websocket.send(self.make_json(channel, ping + 'What is your role?'))
-									elif step == 3:
-										user.role = text
-										await websocket.send(self.make_json(channel, ping + 'Registration complete! Name: {}, Manager: {}, Team: {}, Role: {}'.format(user.name, user.manager, user.team, user.role)))
-										user.state = ''
-									user.step += 1
-								elif user.state == 'timesheet-init':
-									if text == 'yes':
-										date = datetime.date.today()
-										new_date = lastday(date, 'sunday')
-										path = self.generate_default(user.name, new_date)
-										send_email(user.name,new_date, 'myTimeSheet.xlsx', path, user.manager)
-										await websocket.send(self.make_json(channel, ping + 'BOOM! Your timesheet is sent'))
-									else:
-										args = text.split(' ')
-										if len(args) != 6:
-											await websocket.send(self.make_json(channel, ping + 'Sorry, wrong syntax. Please start over'))
-										else:
-											path = generate_specific(user.name, args[0], args[1], args[2], args[3], args[4], args[5])
-											date = datetime.datetime.strptime(args[0], '%m-%d-%Y')
-											send_email(user.name, lastday(date, 'sunday'), 'myTimeSheet.xlsx', path, user.manager)
-									user.state = ''
+        asyncio.get_event_loop().run_until_complete(main())
 
-		asyncio.get_event_loop().run_until_complete(main())
+    def get_user_name(self, internal_name):
+        return self.user_map[internal_name]
 
-	def get_user_name(self, internal_name):
-		return self.user_map[internal_name]
+    def generate_default(self, fullname, date):
+        path = generate_specific(fullname, date, 8, 8, 8, 8, 8)
+        return path
 
-	def generate_default(self, fullname, date):
-		path = generate_specific(fullname, date, 8, 8, 8, 8, 8)
-		return path
+    def get_date_from_secs(self, epoch_secs):
+        ts = time.gmtime(epoch_secs)
+        mon, day, year = ts.tm_mon, ts.tm_mday, ts.tm_year
+        if mon < 10:
+            mon = '0' + str(mon)
+        if day < 10:
+            day = '0' + str(day)
+        return '{0}-{1}-{2}'.format(mon, day, year)
 
-	def get_date_from_secs(self, epoch_secs):
-		ts = time.gmtime(epoch_secs)
-		mon, day, year = ts.tm_mon, ts.tm_mday, ts.tm_year
-		if mon < 10:
-			mon = '0' + str(mon)
-		if day < 10:
-			day = '0' + str(day)
-		return '{0}-{1}-{2}'.format(mon, day, year)
+    def make_json(self, channel, text):
+        return json.dumps({'type':'message', 'text':text, 'channel':channel})
 
-	def make_json(self, channel, text):
-		return json.dumps({'type':'message', 'text':text, 'channel':channel})
-
-	def switch_state(self, text, name):
-		state = ''
-		if 'timesheet' in text:
-			new_text = name + 'Would you like to use defaults? If not specify the date' + \
-					'[mm-dd-yyyy] and hours worked each day separated by spaces.'
-			state = 'timesheet-init'
-		elif 'register me' in text:
-			new_text = name + 'You have already registered...'
-		elif text[len(text) - 1] == '?':
-			new_text = name + 'I noticed you sent a question, let me think...'
-			state = 'faq'
-		else:
-			new_text = name + 'Sorry I\'m too dumb to understand'
-		return state, new_text
+    def switch_state(self, text, name):
+        state = ''
+        if 'timesheet' in text:
+            new_text = name + 'Would you like to use defaults? If not specify the date' + \
+                    '[mm-dd-yyyy] and hours worked each day separated by spaces.'
+            state = 'timesheet-init'
+        elif 'register me' in text:
+            new_text = name + 'You have already registered...'
+        elif text[len(text) - 1] == '?':
+            new_text = name + 'I noticed you sent a question, let me think...'
+            state = 'faq'
+        else:
+            new_text = name + 'Sorry I\'m too dumb to understand'
+        return state, new_text
 
 l = Listener()
 l.start()
--- chrono.py
+++ chrono.py
@@ -10,64 +10,63 @@ logs = db.logs
 faq = db.faq
 
 def log_activity(user, log):
-	return log_activity_time(user, datetime.datetime.utcnow(), log)
+    return log_activity_time(user, datetime.datetime.utcnow(), log)
 
 def log_activity_time(user, date_time, log):
-	post = {'author': user,
-			'date': date_time,
-			'log': log}
-	post_id = logs.insert_one(post).inserted_id
-	return post_id
+    post = {'author': user,
+            'date': date_time,
+            'log': log}
+    post_id = logs.insert_one(post).inserted_id
+    return post_id
 
 def find_latest_datetime(user, date_time):
-	date_beginning = datetime.datetime(date_time.year, date_time.month, date_time.day)
-	if user:
-		posts = logs.find({'author' : user, \
-				'date' : {'$lt': date_time, '$gt': date_beginning}}).sort('date')
-	else:
-		posts = logs.find({'date' : {'$lt': date_time, '$gt': date_beginning}})
-								.sort('date')
-	latest = posts[posts.count() - 1]
-	return latest
+    date_beginning = datetime.datetime(date_time.year, date_time.month, date_time.day)
+    if user:
+        posts = logs.find({'author' : user, \
+                'date' : {'$lt': date_time, '$gt': date_beginning}}).sort('date')
+    else:
+        posts = logs.find({'date' : {'$lt': date_time, '$gt': date_beginning}}).sort('date')
+    latest = posts[posts.count() - 1]
+    return latest
 
 def find_latest(user):
-	return find_latest_datetime(user, datetime.datetime.utcnow())
+    return find_latest_datetime(user, datetime.datetime.utcnow())
 
 def find_latest_rand():
-	return find_latest_datetime(None, datetime.datetime.utcnow())
+    return find_latest_datetime(None, datetime.datetime.utcnow())
 
 def distance(q1, q2):
-	if q1 == q2:
-		return 0
-	return similarity(q1, q2)
+    if q1 == q2:
+        return 0
+    return similarity(q1, q2)
 
 def get_answer(question, threshold=3):
-	"""This function gets the closest answer to a question. The question param is a string
-	This function returns None if there is not a question similar enough.
-	"""
-	cursor = faq.find()
-	min_dist, best_post = float('inf'), None
-	for post in cursor:
-		new_dist = distance(question, post['question'])
-		if new_dist < min_dist:
-			min_dist, best_post = new_dist, post
-	return best_post['answer'] if min_dist < threshold else None
+    """This function gets the closest answer to a question. The question param is a string
+    This function returns None if there is not a question similar enough.
+    """
+    cursor = faq.find()
+    min_dist, best_post = float('inf'), None
+    for post in cursor:
+        new_dist = distance(question, post['question'])
+        if new_dist < min_dist:
+            min_dist, best_post = new_dist, post
+    return best_post['answer'] if min_dist < threshold else None
 
 def set_answer(question, answer, user):
-	"""This function sets a question to an answer. user param means whoever answered it"""
-	date_time = datetime.datetime.utcnow();
-	cursor = faq.find({'question' : question})
-	if cursor.count() > 0:
-		update_result = faq.update_one({'question': question}, {
-			'$set' : {
-				'answer' : answer,
-				'user' : user,
-				'last_modified' : date_time}})
-		return update_result.modified_count() > 0
-	post = {'question': question,
-			'answer': answer,
-			'last_modified': date_time,
-			'user': user}
-	post_id = faq.insert_one(post).inserted_id
-	return True
+    """This function sets a question to an answer. user param means whoever answered it"""
+    date_time = datetime.datetime.utcnow();
+    cursor = faq.find({'question' : question})
+    if cursor.count() > 0:
+        update_result = faq.update_one({'question': question}, {
+            '$set' : {
+                'answer' : answer,
+                'user' : user,
+                'last_modified' : date_time}})
+        return update_result.modified_count() > 0
+    post = {'question': question,
+            'answer': answer,
+            'last_modified': date_time,
+            'user': user}
+    post_id = faq.insert_one(post).inserted_id
+    return True
 
