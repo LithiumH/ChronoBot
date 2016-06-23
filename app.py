372eb1ce9a55470bdb11768b84510ba4a360bbb3
Haojun Li <haojun.li@appdynamics.com>
2016-06-23 12:10:20 -0700

    Partially added support for register

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from chrono import *
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ def hello():
 @app.route('/generate', methods=['POST'])
 def generate():
 	return ''
+
+@app.route('/log', methods=['POST'])
+def log():
+	return ''
+
+@app.route('/register_intern', methods=['POST'])
+def register():
+	return ''
+
 
 if __name__ == "__main__":
 	app.run()
--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from constants import *
 class Listener(object):
 	def __init__(self):
 		self.state = '' # state can be timesheet-init, or an empty string
+		self.user_map = user_map
 
 	def start(self):
 		token = 'xoxb-53468281812-uwQohEw49nWfhcy8N2myHv8H'
@@ -25,38 +26,38 @@ class Listener(object):
 					greeting = await websocket.recv()
 					message = json.loads(greeting)
 					print(message)
-					if 'type' in message and message['type'] == 'message' and message['user'] in user_map:
+					if 'type' in message and message['type'] == 'message' and message['user'] in self.user_map:
 						text = message['text']
+						name = self.get_user_name(message['user'])
 						channel = message['channel']
-						if '<@U1KDS89PW>' in text:
-							if 'timesheet' in text and not self.state:
-								new_text = 'Would you like to use defaults? If not specify the date [mm-dd-yyyy] and hours worked each day separated by spaces.'
-								self.state = 'timesheet-init'
-								await websocket.send(self.make_res_json(channel, new_text))
-							else:
-								await websocket.send(self.make_res_json(channel, 'Sorry I\'m too dumb to understand'))
+						if '<@U1KDS89PW>' in text and not self.state:
+							new_text, self.state = self.switch_state(text)
+							await websocket.send(self.make_json(channel, new_text))
 						elif self.state == 'timesheet-init':
-							name = self.get_user_name(message['user'])
 							if text == 'yes':
 								date = datetime.date.today()
 								path = self.generate_default(name, '{0}-{1}-{2}'.format(date.month, date.day, date.year))
 								send_email(lastday(date, 'sunday'), 'myTimeSheet.xlsx', path)
-								await websocket.send(self.make_res_json(channel, 'BOOM! Your timesheet is sent'))
+								await websocket.send(self.make_json(channel, 'BOOM! Your timesheet is sent'))
 							else:
 								args = text.split(' ')
 								if len(args) != 6:
-									await websocket.send(self.make_res_json(channel, 'Sorry, wrong syntax. Please start over'))
+									await websocket.send(self.make_json(channel, 'Sorry, wrong syntax. Please start over'))
 								else:
 									path = generate_specific(name, args[0], args[1], args[2], args[3], args[4], args[5])
 									date = datetime.datetime.strptime(args[0], '%m-%d-%Y')
 									send_email(name, lastday(date, 'sunday'), 'myTimeSheet.xlsx', path)
-									await websocket.send(self.make_res_json(channel, 'BOOM! Your timesheet is sent'))
+									await websocket.send(self.make_json(channel, 'BOOM! Your timesheet is sent'))
 							self.state = ''
+						elif self.state == 'register me':
+							pass # TODO
+
+
 
 		asyncio.get_event_loop().run_until_complete(main())
 
 	def get_user_name(self, internal_name):
-		return user_map[internal_name]
+		return self.user_map[internal_name]
 
 	def generate_default(self, fullname, date):
 		path = generate_specific(fullname, date, 8, 8, 8, 8, 8)
@@ -71,8 +72,22 @@ class Listener(object):
 			day = '0' + str(day)
 		return '{0}-{1}-{2}'.format(mon, day, year)
 
-	def make_res_json(self, channel, text):
+	def make_json(self, channel, text):
 		return json.dumps({'type':'message', 'text':text, 'channel':channel})
+
+	def switch_state(self, text):
+		state = ''
+		if 'timesheet' in text:
+			new_text = 'Would you like to use defaults? If not specify the date' + \
+					'[mm-dd-yyyy] and hours worked each day separated by spaces.'
+			state = 'timesheet-init'
+		elif 'register me' in text:
+			new_text = 'What is your full name?'
+			state = 'register me'
+		else:
+			new_text = 'Sorry I\'m too dumb to understand'
+		return state, new_text
+
 
 l = Listener()
 l.start()
