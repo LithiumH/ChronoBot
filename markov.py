0bed05fe629151a7f1d9d3b52c63f6de24a8cf3e
Haojun Li <haojun.li@appdynamics.com>
2016-06-24 10:48:41 -0700

    changed markov to be more intelligent

--- markov.py
+++ markov.py
@@ -14,7 +14,14 @@ convs = db.convs
 
 def get_conv(p1='', p2='', p3=''):
 	cursor = convs.find({'p3':p3, 'p2':p2, 'p1':p1})
-	print(p1)
+	if (cursor.count() > 0):
+		index = rand.randint(0, cursor.count()-1)
+		return cursor[index]['response']
+	cursor = convs.find({'p3':'', 'p2':p2, 'p1':p1})
+	if (cursor.count() > 0):
+		index = rand.randint(0, cursor.count()-1)
+		return cursor[index]['response']
+	cursor = convs.find({'p3':'', 'p2':'', 'p1':p1})
 	if (cursor.count() > 0):
 		index = rand.randint(0, cursor.count()-1)
 		return cursor[index]['response']
